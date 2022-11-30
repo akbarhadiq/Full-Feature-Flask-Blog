c1fad7808263350dcf5c0aa4bde4cd18f8620110
akbarhadiq <akbarhadiq.ha@gmail.com>
2022-11-30 21:07:30 +0700

    edit webpages to use a singular layout file (jinja3)

--- server.py
+++ server.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+posts = [
+    {
+        'author': 'Akbar Hadiq',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+
+    {
+        'author': 'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 # you can use double routes
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 # flask auto run when script run:
 if __name__ == "__main__":
